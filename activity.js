e91e09a866cd312883759e67e5dbe248f41b3afc
ElidaVelasquez <mariaevelasquezm24@gimail.com>
2023-08-01 19:35:14 -0500

    actividad 1

---
+++ activity.js
@@ -0,0 +1,7 @@
+let name = "jonny",
+lastname = "gil",
+age = 17,
+yearofbirth = 2006,
+city = "medellin" ;
+
+console.log("mi nombre es " + name + ", naci en el " + yearofbirth + " tengo " + age + "años de edad. " + "yo soy de" + city);
